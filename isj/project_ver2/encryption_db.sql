0feb1474775f79f8a57937342957ab5c6c36c85c
2thereal <kcw740707@gmail.com>
2025-03-24 00:14:31 +0900

    Delete isj/project_ver2 directory

--- isj/project_ver2/encryption_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS encryption_db;
-USE encryption_db;
-
-CREATE TABLE encrypted_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_plain VARCHAR(255),
-    encrypted TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
