02015e26f327208caa19899dfb9e6babc8562ed4
kor-BaKi <rlaehd112830@naver.com>
2025-11-14 18:50:50 +0900

    fix: remove build artifacts and update .gitignore

---
+++ backend/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/baki/Desktop/final/capstone/backend/database.cpp" "CMakeFiles/main.dir/database.cpp.o" "gcc" "CMakeFiles/main.dir/database.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/handler.cpp" "CMakeFiles/main.dir/handler.cpp.o" "gcc" "CMakeFiles/main.dir/handler.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/leak.cpp" "CMakeFiles/main.dir/leak.cpp.o" "gcc" "CMakeFiles/main.dir/leak.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/old.cpp" "CMakeFiles/main.dir/old.cpp.o" "gcc" "CMakeFiles/main.dir/old.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/reuse.cpp" "CMakeFiles/main.dir/reuse.cpp.o" "gcc" "CMakeFiles/main.dir/reuse.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/utils.cpp" "CMakeFiles/main.dir/utils.cpp.o" "gcc" "CMakeFiles/main.dir/utils.cpp.o.d"
+  "/Users/baki/Desktop/final/capstone/backend/vuln.cpp" "CMakeFiles/main.dir/vuln.cpp.o" "gcc" "CMakeFiles/main.dir/vuln.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
